139927cd75f1618df8aa0b94db5c2d3556ae37c3
MarianaCarlo <luzmarianacarlo1999@gmail.com>
2022-07-25 12:18:44 -0400

    first commit

---
+++ src/main/java/com/bisaseg/ms/reinsurance/BisasegMsReinsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.bisaseg.ms.reinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BisasegMsReinsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BisasegMsReinsuranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bisaseg/ms/reinsurance/BisasegMsReinsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bisaseg.ms.reinsurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BisasegMsReinsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
